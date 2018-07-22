8c50f004788cf509ee9432a292d7fdf65e0a8495
Aditya Kuppa <adityakuppa27@gmail.com>
2018-07-22 13:06:50 +0530

    Update CuttingRod.cpp

--- DynamicProgramming/CuttingRod.cpp
+++ DynamicProgramming/CuttingRod.cpp
@@ -1,13 +1,13 @@
 /* Given a rod of length n inches and an array of prices that contains prices of all pieces of size smaller than n. Determine the maximum value obtainable by cutting up the rod and selling the pieces. */
 
-int rodcut(int price[], int l)
+int rodcut(int price[], int n)
 {
-  int 1,j;
+  int i,j;
   int values[n+1];
   values[0]=1;
   for(i=1;i<=n;i++)
   {
-    int max_values=INT_MIN;
+    int max_value=INT_MIN;
     for(j=0;j<i;j++)
     {
       max_value=max(max_value,price[j]+values[i-j-1]);
